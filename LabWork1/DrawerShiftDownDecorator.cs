1227470500ac2e45dbf9dbbe08b0425e52d246ef
prosto-vorobey <100966808+prosto-vorobey@users.noreply.github.com>
2023-12-20 02:50:03 +0300

    Создание и отображение Наблюдателя. Создание класс Субъекта. И соответствующих конкретных классов для матрицы.

---
+++ LabWork1/DrawerShiftDownDecorator.cs
@@ -0,0 +1,28 @@
+﻿public class DrawerShiftDownDecorator : IDrawer
+{
+    IDrawer _drawer;
+    int _shift;
+    public DrawerShiftDownDecorator(IDrawer drawer, int shift)
+    {
+        _drawer = drawer;
+        _shift = shift;
+
+    }
+    public void DrawBorder(int numCols, int numRows, int maxValLength)
+    {
+        _drawer.DrawBorder(numCols, numRows, maxValLength);
+
+    }
+
+    public void DrawCellBorder(int col, int row, int maxValLength)
+    {
+        _drawer.DrawCellBorder(col, row + _shift, maxValLength);
+
+    }
+
+    public void DrawContent(string cont, int col, int row, int maxValLength)
+    {
+        _drawer.DrawContent(cont, col, row + _shift, maxValLength);
+
+    }
+}
--- LabWork1/MatrixStatistic.cs
+++ LabWork1/MatrixStatistic.cs
@@ -2,32 +2,20 @@
 
 public class MatrixStatistic
 {
-    public int ValSumm
-    {
-        get;
-
-    }
-    public float ValAver
-    {
-        get;
-
-    }
-    public int ValMax
-    {
-        get;
-
-    }
-    public int ValNotNull
-    {
-        get;
-
-    }
+    public int ValSumm { get; }
+    public float ValAver { get; }
+    public int ValMax { get; }
+    public int ValNotNull { get; }
+    public int NumColumns { get; }
+    public int NumRows { get; }
     public MatrixStatistic(IMatrix matrix)
     {
         int count = 0;
-        for (int i = 0; i < matrix.NumColumns; i++)
+        NumColumns = matrix.NumColumns;
+        NumRows = matrix.NumRows;
+        for (int i = 0; i < NumColumns; i++)
         {
-            for (int j = 0; j < matrix.NumRows; j++)
+            for (int j = 0; j < NumRows; j++)
             {
                 int val = matrix.Get(i, j);
                 ValSumm += val;
